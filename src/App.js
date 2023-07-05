5b19649e4a5a5015c75c5cdb1acf0dc1fb8e58ba
michel4alves <michelsantosa@gmail.com>
2023-07-05 12:27:10 -0300

    backup 04/07

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import Cardapio from "./pages/Cardapio";
 import Delivery from "./pages/Delivery";
 import Contato from "./pages/Contato";
 import NavMenu from "./components/navbar/NavMenu";
+import Container from "./components/container/Container";
 
 
 export default function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <header className="header">
                     <NavMenu />
+                </header>
+                <Container>
                     <Routes>
                         <Route path='/' element={<Home />} />
                         <Route path='cardapio' element={<Cardapio />} />
                         <Route path='delivery' element={<Delivery />} />
                         <Route path='contato' element={<Contato />} />
                     </Routes>
+                </Container>
                 </BrowserRouter>
         </div>
     );
