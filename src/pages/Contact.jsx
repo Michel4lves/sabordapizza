051c4e2aee4a4d50ca244b0afd27530252bf0694
Michel4lves <michelsantosa@gmail.com>
2023-07-12 17:52:30 -0300

    contact page begin

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,27 @@
+import Container from "../components/container/Container";
+
+import logoGray from "../img/logo-gray.png"
+
+import "../sass/pages/Contact.sass"
+
 export default function Contact() {
     return (
         <div>
-            <h1>contato</h1>
+            <Container customClass="center-container">
+                <h1 className="contact-title">Fale conosco</h1>
+                <div className="contacts">
+                    <div className="contact-info">
+                        <img src={logoGray} alt="logo-gray" />
+                        <p>(53) <span>3210-5679</span></p>
+                        <h2>Email:</h2>
+                        <p>contato@sabordapizza.com.br</p>
+                    </div>
+                    <span className="bar"></span>
+                    <div className="contact-form">
+                        <p>FORM</p>
+                    </div>
+                </div>
+            </Container>
         </div>
     )
 }
